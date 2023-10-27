72ce09748c6bd0980d1a83a53a01ca7df24ac8ae
Michael Epelboim <mepelboim@outlook.com>
2023-10-27 16:13:40 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/rick-morty-characters/",
+  base: "/rick-morty-characters",
   plugins: [react()],
   resolve: {},
 });
